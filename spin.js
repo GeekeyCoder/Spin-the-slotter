d0e57c0bcc7d906e6564c61f9be177ccde7c1d4f
GeekeyCoder <geekycoder886@gmail.com>
2023-04-15 00:24:40 +0530

    Add files via uplaod

---
+++ spin.js
@@ -0,0 +1,8 @@
+// 1. Deposite some money
+// 2. determine number of lines to bwt on
+// 3. collect a bet amount
+// 4. spin the slot machine
+// 5. check if the user won
+// 6. give the user their winnings
+// 7. play again
+
